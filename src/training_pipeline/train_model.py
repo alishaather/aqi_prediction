20a4c09d8a06bc7ff66b8ba39c7009b9bd82cbb3
Alisha <alishaather27@gmail.com>
2026-08-26 19:59:42 +0500

    reorganize src folder structure

---
+++ src/training_pipeline/train_model.py
@@ -0,0 +1,8 @@
+import os
+import joblib
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
